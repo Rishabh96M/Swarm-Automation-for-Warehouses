193b7870b1d15674500d13ae0dfa308340d66b2c
Rishabh Mukund <rishabh.m96@gmail.com>
2021-12-11 22:57:27 -0500

    modified assign_robots_to_crate method in the swarmmaster header and source file respectively

--- include/swarm_master/swarm_master.hpp
+++ include/swarm_master/swarm_master.hpp
@@ -11,9 +11,12 @@
 
 #pragma once
 
+#include <set>
+#include <memory>
 #include <vector>
 #include <queue>
 #include <utility>
+#include <unordered_map>
 #include "./site.hpp"
 #include "../structs/task.hpp"
 #include "../structs/crate.hpp"
@@ -22,16 +25,36 @@
 #include "./assignment_designator.hpp"
 
 class SwarmMaster {
- protected:
+ private:
+  AssignmentDesignator* designator;
+  bool swarm_is_occupied{false};
   double weight_per_robot;            // kg
+  int required_robots_system{};
   std::vector<int> assigned_ids{};
-  std::vector<Robot> robots_avail{};
   std::vector<int> assigned_site_id{};
-  std::vector<Site> sites{};
+  std::unordered_map<int, Robot> robots_avail{};
+  std::unordered_map<int, Site> sites{};
+  std::unordered_map<int, std::set<int>> robots_at_site_waiting;
+
+  /**
+   * @brief Clear crates from vect
+   * 
+   */
+  void clear_sites();
+
+  /**
+   * @brief Clear robots from vec
+   * 
+   */
+  void clear_robots();
+
  public:
-  SwarmMaster(double _weight_per_robot=2.0) :
-    weight_per_robot{_weight_per_robot} {}
-  ~SwarmMaster() {}
+  SwarmMaster(AssignmentDesignator* _designator,
+              double _weight_per_robot=2.0) :
+      weight_per_robot{_weight_per_robot},
+      designator{_designator} {
+    if (_weight_per_robot <= 0) throw std::invalid_argument("Robot strength must be greater than 0.");
+  }
 
   /**
   * @brief Add robot to swarm
@@ -45,7 +68,15 @@ class SwarmMaster {
   * @brief Assign crates as sites
   * 
   */
-  void assign_crate(Crate crate);
+  int add_crate_to_system(Crate crate);
+
+  /**
+   * @brief Check if there are enough robots for crate reqs
+   * 
+   * @return true 
+   * @return false 
+   */
+  bool enough_robots_for_assignments();
 
   /**
   * @brief Find the relative positions of each robot in the crate frame
@@ -53,34 +84,62 @@ class SwarmMaster {
   * @param site 
   * @return std::vector<std::array<double, 2>> 
   */
-  std::vector<std::array<double, 2>> assign_robots_along_crate(const Site& site);
+  std::vector<std::array<double, 3> > assign_robots_along_crate(const Site& site);
 
   /**
   * @brief Assign all robots to designated crates
   * 
   * @return std::vector<Assignment> 
   */
-  std::vector<Assignment> assign_robots_to_crates();
+  std::shared_ptr<std::vector<Assignment> > assign_robots_to_crates();
 
   /**
-  * @brief Grab next task from task queue and get it done.
-  * 
-  * @return true 
-  * @return false 
-  */
-  bool perform_next_task();
+   * @brief Break down assignment into list of tasks
+   * 
+   * @return std::vector<Task> 
+   */
+  std::shared_ptr<std::vector<Task> > break_down_assignment(const Assignment& assignment);
+
+  /**
+   * @brief Check if all robots have signaled that they are waiting
+   * 
+   * @param robot_id 
+   * @param site_id 
+   * @return true 
+   * @return false 
+   */
+  std::pair<bool, int> all_robots_at_site_waiting(int robot_id);
+
+  /**
+   * @brief Clears all task related vars
+   * 
+   */
+  void clear_tasks();
+
+  /**
+   * @brief Resets swarm
+   * 
+   */
+  void reset_swarm();
 
   /**
   * @brief Get avail robots
   * 
   * @return std::pair<std::vector<int>, std::vector<Robot>> 
   */
-  const std::pair<std::vector<int>, std::vector<Robot> > get_avail_robots();
+  const std::unordered_map<int, Robot> get_avail_robots();
 
   /**
   * @brief Get assigned sites
   * 
   * @return const std::vector<Site>&
   */
-  const std::vector<Site>& get_sites();
+  const std::unordered_map<int, Site>& get_sites();
+
+  /**
+   * @brief Get the num robots required object
+   * 
+   * @return int 
+   */
+  int get_num_robots_required();
 };
--- src/swarm_master/swarm_master.cpp
+++ src/swarm_master/swarm_master.cpp
@@ -9,6 +9,8 @@
  * 
  */
 
+#include <string>
+#include <memory>
 #include <vector>
 #include <utility>
 #include <stdexcept>
@@ -20,62 +22,148 @@ int SwarmMaster::add_robot_to_swarm(std::array<double, 2> pos_init) {
     if (assigned_ids.empty()) id = 0;
     else id = *(assigned_ids.end()-1) + 1;
     assigned_ids.push_back(id);
-    robots_avail.push_back({id, pos_init});
+    robots_avail.emplace(id, Robot(id, pos_init));
     return id;
 }
 
-void SwarmMaster::assign_crate(Crate crate) {
+int SwarmMaster::add_crate_to_system(Crate crate) {
     int id;
     if (assigned_site_id.empty()) id = 0;
     else id = *(assigned_site_id.end()-1) + 1;
     if (crate.mass > 4 * weight_per_robot)
         throw std::invalid_argument("Crate is too heavy for system!");
-    sites.push_back({id, crate, weight_per_robot});
+    sites.emplace(id, Site(id, crate, weight_per_robot));
+    required_robots_system += sites.at(id).robots_required;
+    robots_at_site_waiting.insert({id, {}});
     assigned_site_id.push_back(id);
+    return id;
 }
 
-std::vector<std::array<double, 2>> SwarmMaster::assign_robots_along_crate(const Site& site) {
+bool SwarmMaster::enough_robots_for_assignments() {
+    return robots_avail.size() >= required_robots_system;
+}
+
+std::vector<std::array<double, 3> > SwarmMaster::assign_robots_along_crate(const Site& site) {
     int num_robots = site.assigned_ids.size();
-    std::vector<std::array<double, 2>> ret;
-    ret.reserve(num_robots);
+    std::vector<std::array<double, 3>> ret;
     auto footprint = site.crate.base_footprint;
-    auto bigger_half_footprint = (footprint[0] > footprint[1] ? footprint[0]/2.0 : footprint[1]/2.0);
-    auto smaller_half_footprint = (footprint[0] < footprint[1] ? footprint[0]/2.0 : footprint[1]/2.0);
+    bool X_longer_than_Y = footprint[0] > footprint[1];
+    double half_x = footprint[0]/2.0;
+    double half_y = footprint[1]/2.0;
 
     if (num_robots == 2) {
-        ret.push_back({bigger_half_footprint, 0});
-        ret.push_back({-bigger_half_footprint, 0});
+        if (X_longer_than_Y) {
+            ret.push_back({half_x, 0, 180});
+            ret.push_back({-half_x, 0, 0});
+        } else {
+            ret.push_back({0, half_y, 270});
+            ret.push_back({0, -half_y, 90});
+        }
     } else if (num_robots == 3) {
-        ret.push_back({bigger_half_footprint-0.5, smaller_half_footprint});
-        ret.push_back({bigger_half_footprint-0.5, -smaller_half_footprint});
-        ret.push_back({-bigger_half_footprint, 0});
+        if (X_longer_than_Y) {
+            ret.push_back({half_x-0.2, half_y, 270});
+            ret.push_back({half_x-0.2, -half_y, 90});
+            ret.push_back({-half_x, 0, 0});
+        } else {
+            ret.push_back({half_x, half_y-0.2, 180});
+            ret.push_back({-half_x, half_y-0.2, 0});
+            ret.push_back({0, -half_y, 90});
+        }
     } else if (num_robots == 4) {
-        ret.push_back({bigger_half_footprint, 0});
-        ret.push_back({-bigger_half_footprint, 0});
-        ret.push_back({0, smaller_half_footprint});
-        ret.push_back({0, -smaller_half_footprint});
+        ret.push_back({half_x, 0, 180});
+        ret.push_back({-half_x, 0, 0});
+        ret.push_back({0, half_y, 270});
+        ret.push_back({0, -half_y, 90});
     } else {
-        throw std::invalid_argument("There must be fewer than 4 robots. We cannot lift this crate!");
+        throw std::invalid_argument("There must be fewer than 5 robots. We cannot lift this crate!");
     }
+
     return ret;
 }
 
-std::vector<Assignment> SwarmMaster::assign_robots_to_crates() {
-    GrowingRadiusDesignator designator(sites, robots_avail);
-    auto designations = designator.get_designations();
-    std::vector<Assignment> assignments;
-    for (const auto& designation : *designations) {
-        auto along_crate = assign_robots_along_crate(designation);
-        for (std::size_t i=0; i<designation.assigned_ids.size(); i++)            
-            assignments.push_back({designation.assigned_ids[i], designation.crate, along_crate[i]});
+std::shared_ptr<std::vector<Assignment> > SwarmMaster::assign_robots_to_crates() {
+    std::shared_ptr<std::vector<Assignment> > assignments = std::make_shared<std::vector<Assignment> >();
+    if (!swarm_is_occupied && enough_robots_for_assignments()) {
+        swarm_is_occupied = true;
+        auto designations = designator->get_designations(sites, robots_avail);
+        for (const auto& designation : *designations) {
+            auto along_crate = assign_robots_along_crate(designation);
+            for (std::size_t i=0; i<designation.assigned_ids.size(); i++) {
+                int robot_id = designation.assigned_ids[i];
+                robots_avail.at(robot_id).assigned_site = designation.site_id;
+                assignments->push_back({robot_id, designation.site_id,
+                            designation.crate, along_crate[i]});
+            }
+        }
     }
     return assignments;
 }
 
-const std::pair<std::vector<int>, std::vector<Robot> > SwarmMaster::get_avail_robots() {
-    return std::make_pair(assigned_ids, robots_avail);
+std::shared_ptr<std::vector<Task> > SwarmMaster::break_down_assignment(const Assignment& assignment) {
+    std::shared_ptr<std::vector<Task> > ret = std::make_shared<std::vector<Task> >();
+    typedef std::unordered_map<std::string, double> commandDict;
+    ret->push_back({Task::MvPlatform, commandDict{{"PlatformHeight", assignment.crate.start_pos[2]-0.05}}});
+
+    auto toX_begin = assignment.crate.start_pos[0] + assignment.pos_crate_frame[0];
+    auto toY_begin = assignment.crate.start_pos[1] + assignment.pos_crate_frame[1];
+
+    ret->push_back({Task::Drive, commandDict{{"ToX", toX_begin}, {"ToY", toY_begin}, {"ToTheta", assignment.pos_crate_frame[2]}}});
+    ret->push_back({Task::MvPlatform, commandDict{{"PlatformHeight", assignment.crate.goal_pos[2]}}});
+    ret->push_back({Task::Wait, commandDict{{"AssignmentID", assignment.site_id}}});
+
+    auto toX_end = assignment.crate.goal_pos[0] + assignment.pos_crate_frame[0];
+    auto toY_end = assignment.crate.goal_pos[1] + assignment.pos_crate_frame[1];
+    ret->push_back({Task::Drive, commandDict{{"ToX", toX_end}, {"ToY", toY_end}, {"ToTheta", assignment.pos_crate_frame[2]}}});
+    ret->push_back({Task::Wait, commandDict{{"AssignmentID", assignment.site_id}}});
+    ret->push_back({Task::MvPlatform, commandDict{{"PlatformHeight", assignment.crate.goal_pos[2]-0.05}}});
+    return ret;
 }
 
-const std::vector<Site>& SwarmMaster::get_sites() {
+std::pair<bool, int> SwarmMaster::all_robots_at_site_waiting(int robot_id) {
+    int site_id = robots_avail.at(robot_id).assigned_site;
+    robots_at_site_waiting.at(site_id).insert(robot_id);
+    if (robots_at_site_waiting.at(site_id).size() == sites.at(site_id).robots_required) {
+        robots_at_site_waiting.at(site_id).clear();
+        return std::make_pair(true, site_id);
+    }
+    return std::make_pair(false, site_id);
+}
+
+void SwarmMaster::clear_sites() {
+    sites.clear();
+    assigned_site_id.clear();
+    required_robots_system = 0;
+    robots_at_site_waiting.clear();
+}
+
+void SwarmMaster::clear_robots() {
+    robots_avail.clear();
+    assigned_ids.clear();
+}
+
+void SwarmMaster::clear_tasks() {
+    clear_sites();
+    swarm_is_occupied = false;
+}
+
+void SwarmMaster::reset_swarm() {
+    clear_tasks();
+    clear_robots();
+}
+
+const std::unordered_map<int, Robot> SwarmMaster::get_avail_robots() {
+    return robots_avail;
+}
+
+const std::unordered_map<int, Site>& SwarmMaster::get_sites() {
+    if (sites.size() != assigned_site_id.size() ||
+        sites.size() != robots_at_site_waiting.size()) 
+            throw std::length_error("Sites not initialized correctly.");
     return sites;
+}
+
+int SwarmMaster::get_num_robots_required() {
+    if (robots_avail.size() != assigned_ids.size())
+        throw std::length_error("Robots not initialized correctly.");
+    return required_robots_system;
 }
